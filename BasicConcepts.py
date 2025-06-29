0fc1acf8598981ebb7f296a488ec13bfc6ae54f6
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-06-29 19:52:19 +0200

    Update BasicConcepts.py

--- BasicConcepts.py
+++ BasicConcepts.py
@@ -1,1 +1,23 @@
 #Basic Concepts
+#Classes & Objects
+
+class school:
+    name1 = ""
+    name2 = ""
+       
+    def teacher(self):
+        print("The teacher name is very good")
+    
+
+first_teacher = school
+second_teacher = school
+
+first_teacher.name1 = "TOM VICTOR"
+second_teacher.name2 = "ABDUL"
+
+print(first_teacher.name1)
+print(second_teacher.name2)
+
+
+
+
