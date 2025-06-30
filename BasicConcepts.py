81815cf29a2a8881126856feaad6b56743d0f41b
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-06-30 22:11:50 +0200

    changed the file

--- BasicConcepts.py
+++ BasicConcepts.py
@@ -1,4 +1,23 @@
 #Basic Concepts
+
+#Understanding the data types
+
+#integers, float, boolean, complex
+
+a=2
+b=3.5
+
+sum = a + b
+print(sum)
+
+a = 10
+b = 30
+if a < 30:
+    print(True)
+else:
+    print(Flase)
+
+
 #Classes & Objects
 
 class school:
