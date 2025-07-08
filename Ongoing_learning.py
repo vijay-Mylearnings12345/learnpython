957c8cb25bfe3cde672ffa960fc47b0ed325fa21
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-07-08 08:27:21 +0200

    Update Ongoing_learning.py

--- Ongoing_learning.py
+++ Ongoing_learning.py
@@ -2,9 +2,6 @@ print=("Enter your name, please!")
 name=input()
 print=(f"Hello {name}")
 
-print=("Please, Enter the age"!)
-age= input()
-print = (f"You are {age} years old")
 
 User1_D1 =input("Please Enter your Name")
 User1_D2=input("Please Enter your age")
