aff618d19938c5f90b2f75f2d1c776b2c051157e
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-07-08 08:25:12 +0200

    Update Ongoing_learning.py

--- Ongoing_learning.py
+++ Ongoing_learning.py
@@ -6,3 +6,15 @@ print=("Please, Enter the age"!)
 age= input()
 print = (f"You are {age} years old")
 
+User1_D1 =input("Please Enter your Name")
+User1_D2=input("Please Enter your age")
+User2_D1 =input("Please Enter your Name")
+User2_D2=input("Please Enter your age")
+if User1_D2>User2_D2:
+    print("User1 is Elder! and He/She is the Senior!")
+else:
+    print("User2 is Elder! and He/She is the Senior!")
+    
+ 
+
+
