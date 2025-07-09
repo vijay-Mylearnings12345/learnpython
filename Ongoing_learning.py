7b4ee6e1e16c463471ebc582752cb26b96d82341
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-07-09 18:07:47 +0200

    Update Ongoing_learning.py

--- Ongoing_learning.py
+++ Ongoing_learning.py
@@ -1,23 +1,7 @@
-print=("Enter your name, please!")
-name=input()
-print=(f"Hello {name}")
+num1=input("Enter the First Number")
+num2=input("Enter the secod Number")
+sum = num1 + num2
+print("The sume is", sum)
 
 
-User1_D1 =input("Please Enter your Name")
-User1_D2=input("Please Enter your age")
-User2_D1 =input("Please Enter your Name")
-User2_D2=input("Please Enter your age")
-if User1_D2>User2_D2:
-    print("User1 is Elder! and He/She is the Senior!")
-else:
-    print("User2 is Elder! and He/She is the Senior!")
-    
- 
-a=40
-b=40
-if a!=b:
-  print("Correct")
-else:
-  print("Nothing")
-  
   
