180d933bea035d8fd3d5fe40f5d8621aaf8d427b
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-07-10 07:55:50 +0200

    Update Ongoing_learning.py

--- Ongoing_learning.py
+++ Ongoing_learning.py
@@ -1,7 +1,26 @@
-num1=input("Enter the First Number")
-num2=input("Enter the secod Number")
-sum = num1 + num2
-print("The sume is", sum)
+#Constructing the interger, Float and string
+
+x = int(1)
+y = int(2.5)
+z =int("7.8")
+print(x)
+print(y)
+print(z)
 
 
+
+x = float(1)
+y = float("2.5")
+z =float("0.1")
+print(x)
+print(y)
+print(z)
+
+
+x = str("1")
+y = str(2.5)
+z =str("Test")
+print(x)
+print(y)
+print(z)
   
