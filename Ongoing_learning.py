bb5294b776c576441d8a717f7bf255e947fcdfed
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-07-11 06:54:35 +0200

    Update Ongoing_learning.py

--- Ongoing_learning.py
+++ Ongoing_learning.py
@@ -1,26 +1,8 @@
-#Constructing the interger, Float and string
+#Slicing Strings
 
-x = int(1)
-y = int(2.5)
-z =int("7.8")
-print(x)
-print(y)
-print(z)
+b = "Hello, World!"
+print(b[2:5])
 
+b = "Hello, World!"
+print(b[:5])
 
-
-x = float(1)
-y = float("2.5")
-z =float("0.1")
-print(x)
-print(y)
-print(z)
-
-
-x = str("1")
-y = str(2.5)
-z =str("Test")
-print(x)
-print(y)
-print(z)
-  
