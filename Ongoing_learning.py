d876d86f49e35a98b5c377013a1f2b2cd154cb2a
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-07-21 10:59:09 +0200

    Update Ongoing_learning.py

--- Ongoing_learning.py
+++ Ongoing_learning.py
@@ -1,3 +1,25 @@
-#python environment
-#windows
-#linux
+#Functions
+
+def first_functtion():
+    print("This is not the first function")
+    
+first_function()
+
+def result():
+    a=10
+    b=12.5
+    sum=a+b
+    print(sum)
+    
+result()
+
+def conditional():
+    a=500
+    b=800
+    if a>b:
+        print("a is Bigger value")
+    else:
+        print("a is not the Bigger value")
+        
+conditional()
+        
