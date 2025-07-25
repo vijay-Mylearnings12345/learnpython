48474ab60a6def79d22ff38f80614a89436890c2
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-07-25 17:45:40 +0200

    Update List.py

--- List.py
+++ List.py
@@ -1,5 +1,76 @@
 #List
 
+#Methods - append, copy, clear, count, extend, index, insert, remove, pop, reverese, sort
+
+#Length
+list = [ "Name", "age", "Birthcity", "Nationality", "Marrital Status", 38, True, 1.5]
+print(len(list))
+
+
+#append
+list = [ "Name", "age", "Birthcity", "Nationality", "Marrital Status", 38, True, 1.5]
+list.append("Religion")
+print(list)
+
+#copy & Clear
+list = [ "Name", "age", "Birthcity", "Nationality", "Marrital Status", 38, True, 1.5]
+newlist = list.copy()
+newlist.clear()
+print(list)
+print(newlist)
+
+#Copy & remove
+list = [ "Name", "age", "Birthcity", "Nationality", "Marrital Status", 38, True, 1.5, "Religion"]
+newlist = list.copy()
+newlist.remove("Religion")
+print(list)
+print(newlist)
+
+#Count
+list = [ "Name", "age", "Birthcity", "Nationality", "Marrital Status", 38, True, 1.5, "Religion"]
+newlist = list.count("Name")
+print(newlist)
+
+list = [ "Name", "age", "Birthcity", "Nationality", "Marrital Status", 38, True, 1.5, "Religion"]
+newlist = list.count("a")
+print(newlist)
+
+list = [ "Name", "age", "Birthcity", "Nationality", "Marrital Status", 38, True, 1.5, "Religion"]
+newlist = list.count(38)
+print(newlist)
+
+#Extend
+list = [ "Name", "age", "Birthcity", "Nationality", "Marrital Status", 38, True, 1.5, "Religion"]
+list1 = [ "City", "Workstatus", "Experience"]
+list.extend(list1)
+print(list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 my_list = [1, "text", 2.5, True]
 
 print(my_list)
