eed0887b269e32ca683d56ddf306fd5405f0a72b
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-08-02 18:48:02 +0200

    update

--- Break,Continue,Pass.py
+++ Break,Continue,Pass.py
@@ -1,1 +1,16 @@
-#Brake, continue
+#Brake, continue,pass
+
+x = int(input("Enter the number of bottles required....! "))
+
+av = 5
+i = 1
+
+while i <= x:
+    if i > av:
+        print("Stock finished. Only", av, "bottles available.")
+        break
+    print("Bottle", i)
+    i += 1
+    
+    
+    
