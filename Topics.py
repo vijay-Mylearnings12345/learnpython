afb670de44cccfc9e04616655b5620c22d7e7bf5
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-08-11 18:31:26 +0200

    Create Topics.py

---
+++ Topics.py
@@ -0,0 +1,15 @@
+"""
+Topics to Learn
+"""
+#print
+#variables
+#Datatypes - strings, integer, float, boolean 
+#defining the : f string, place holder while calling the variable
+#Type casting : converting one data type to another data type
+
+
+#Type casting
+
+user1 = int(input("Please enter the name: "))
+print(f" Welcome to the party Mr.{user1}")
+
