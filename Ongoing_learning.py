ddd6e267eb76db65f7471b96476b787cfc62d204
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-08-18 18:34:32 +0200

    update

--- Ongoing_learning.py
+++ Ongoing_learning.py
@@ -1,59 +1,18 @@
-'''This chapter deals with the variables
-All concepts about the variables
-Important to know about declarion of global variables'''
 
-#Variables
-#Variable names
-#Multiple variable values
-#Output Variable
-#Global Variable
+#Abstract base class
 
-a='text'
-print(a)
-b="text"
-print(b)
-c=5
-print(c)
-d=7
-print(d)
-D=7
-print(D)
-
-myvar="string"
-print(type(myvar))
-
-
-x=y=z='vijaykumar'
-print(x)
-print(y)
-print(z)
-
-x,y,z=('vijay','kumar','great')
-print(x)
-print(y)
-print(z)
-
-x = 'awesome'
-def myfunc():
-  x = 'fantastic'
-myfunc()
-print('Python is always ' + x)
-
-x = "Nice"
-def myfunc():
-  global x
-  x = "fantastic"
-myfunc()
-print('Python is always ' +x)
-
-x = 'awesome'
-def myfunc():
-  global x
-  x = 'fantastic'
-myfunc()
-print('Python is ' + x)
-
-
+class gateway():
+    
+    
+    def download():
+        pass
+    
+    def upgrade():
+        pass
+        
+class iotgw():
+    
+    
 
 
 
--- working_file.py
+++ working_file.py
@@ -1,6 +1,27 @@
-value1 = int(input("Please enter your preference (Veg/Non veg)"))
+import requests
 
-if value1 == "Veg":
-    print("available")
+base_url =""
+
+def get_json_info(name):
+    url = f"{base_url}"
+    response = requests.get(url)
+    print(response)
+
+    if response.status_code == 200:
+        print("Retrieved the data")
+    else:
+        print(f"Failed to retrieve the data{response.status_code}")
+
+
+json_name = "iotgw"
+get_json_info(json_name)
+
+
+
+
+
+
+
+
 
 
