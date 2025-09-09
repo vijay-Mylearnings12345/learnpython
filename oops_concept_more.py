f728c3b1adf22077bf8219a2c3fd6a2837e08c41
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2025-09-09 21:53:17 +0200

    Update oops_concept_more.py

--- oops_concept_more.py
+++ oops_concept_more.py
@@ -6,13 +6,19 @@ class Team:
         self.tester = tester
 
     def role1(self):
-        print(f"{self.manager} is taking the Project and Team management responsibility")
+        print(f"{self.po} is taking the Project and Team management responsibility")
 
     
 team1 = Team("Unknown", "Magnus", "Petter", "vijay")
+team2 = Team("Alex", "Marten", "Peter", "Victor")
 
 print(team1.manager)
 print(team1.developer)
 print(team1.tester)
 
+print(team2.manager)
+print(team2.developer)
+print(team2.tester)
+
 team1.role1()
+team2.role1()
