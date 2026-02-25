5b50c6f3f2555feb9824a31f5e30fd511ca961f2
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2026-02-25 13:33:15 +0100

    Update objects_class.py
    made changes in the file

--- objects_class.py
+++ objects_class.py
@@ -4,9 +4,10 @@ class Gateway:
     def __init__(self,name,version,board)
     self.name=name
     self.version=version
+    self.board =board
     
-gateway1=Gateway(EdgeIoT,26.0.1,mind)
-gateway2=Gateway(AristoNG,2.3,compulab)
+gateway1=Gateway(EdgeIoT,26.0.1,mind,)
+gateway2=Gateway(AristoNG,2.3,compulab,)
 gateway3=Gateway(UC,3.3,Beaglebone)
 gateway4=Gateway(Quark,31.0.4,ESAB)
 
