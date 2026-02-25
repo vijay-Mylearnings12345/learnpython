029c6f58c0e2d1b5b70cc22ce6faea63f9258791
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2026-02-25 13:51:43 +0100

    Update objects_class.py
    update the object file

--- objects_class.py
+++ objects_class.py
@@ -1,16 +1,27 @@
 ### Objects & class
 
 class Gateway:
-    def __init__(self,name,version,board)
-    self.name=name
-    self.version=version
-    self.board =board
+    def __init__(self, name, version, board):
+        self.name=name
+        self.version=version
+        self.board = board
+        
+    def flash(self):
+        print(f"This {self.name} gateway is not having the  {self.version} as the image software")
+
+    def update(self):
+        print(f"This {self.name} gateway is with the {self.version} as the mender update software")
     
-gateway1=Gateway(EdgeIoT,26.0.1,mind,)
-gateway2=Gateway(AristoNG,2.3,compulab,)
-gateway3=Gateway(UC,3.3,Beaglebone)
-gateway4=Gateway(Quark,31.0.4,ESAB)
+gateway1=Gateway("EdgeIoT","26.0.1","mind",)
+gateway2=Gateway("AristoNG","2.3","compulab",)
+gateway3=Gateway("UC","3.3","Beaglebone")
+gateway4=Gateway("Quark","31.0.4","ESAB")
+
 
 print(gateway1.name)
 print(gateway1.version)
 print(gateway1.board)
+
+
+gateway1.flash()
+gateway2.update()
