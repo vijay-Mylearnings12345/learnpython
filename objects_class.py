3336f677445f8053a5cbec2067cbc4be6c34510c
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2026-02-25 14:13:22 +0100

    Update objects_class.py
    made changes in the objects_class file

--- objects_class.py
+++ objects_class.py
@@ -1,7 +1,8 @@
 ### Objects & class
 
 class Gateway:
-    def __init__(self, name, version, board):
+    
+    def __init__(self, name, version, board): #constructor
         self.name=name
         self.version=version
         self.board = board
@@ -13,9 +14,9 @@ class Gateway:
         print(f"This {self.name} gateway is with the {self.version} as the mender update software")
     
 gateway1=Gateway("EdgeIoT","26.0.1","mind",)
-gateway2=Gateway("AristoNG","2.3","compulab",)
-gateway3=Gateway("UC","3.3","Beaglebone")
-gateway4=Gateway("Quark","31.0.4","ESAB")
+gatew2212ay=Gateway("AristoNG","2.3","compulab",)
+gatew13ay=Gateway("UC","3.3","Beaglebone")
+gatew14ay=Gateway("Quark","31.0.4","ESAB")
 
 
 print(gateway1.name)
