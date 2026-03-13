486485ba16db1efea0ad9d2c436a6406cd75a650
vijay-Mylearnings12345 <cvijay0410@gmail.com>
2026-03-13 11:33:32 +0100

    Create dict.py
    created the dict file

---
+++ dict.py
@@ -0,0 +1,9 @@
+esab_gw = {"edgegw": "26.0.4", "uc": "3.4", "compulab": "3.0.4"}
+if "edgegw" in esab_gw:
+    print("The software version of edgegw is:", esab_gw["edgegw"])
+if "uc" in esab_gw:
+    print("The software version of uc is:", esab_gw["uc"])
+if "compulab" in esab_gw:
+    print("The software version of compulab is:", esab_gw["compulab"])
+
+    
